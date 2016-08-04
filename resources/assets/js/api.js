234cf7856cbbc7f48be81349bab1659930ad2249
Tyler Huston <thuston@thecargoagency.com>
2016-08-04 17:33:33 -0400

    Remove masonry stuff

--- resources/assets/js/api.js
+++ resources/assets/js/api.js
@@ -50,36 +50,7 @@ var demoList = new Vue({
             });
 
             this.activeEpisode = number;
+
         }
     }
-
-    // methods: {
-    //     fetchDataStart: function () {
-    //
-    //         var apiURL = "https://api.themoviedb.org/3/tv/1399/season/1?api_key=" + apiKeyNew + "&append_to_response=" + "images";
-    //
-    //         this.$http.get( apiURL, function( data ) {
-    //             this.items = data;
-    //             this.episodeNumber = this.items.episodes.length;
-    //         });
-    //     },
-    //
-    //     changeSeason: function (number) {
-    //
-    //         var apiURL = "https://api.themoviedb.org/3/tv/1399/season/1/episode/" + number + "?api_key=" + apiKeyNew + "&append_to_response=" + "images";
-    //
-    //         this.$http.get( apiURL, function( data ) {
-    //             this.items = data;
-    //         });
-    //     },
-    //
-    //     changeEpisode: function (number) {
-    //
-    //         var apiURL = "https://api.themoviedb.org/3/tv/1399/season/1/episode/" + number + "?api_key=" + apiKeyNew + "&append_to_response=" + "images";
-    //
-    //         this.$http.get( apiURL, function( data ) {
-    //             this.items = data;
-    //         });
-    //     }
-    // }
 });
