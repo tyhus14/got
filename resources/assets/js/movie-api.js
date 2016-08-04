815fb765e69151b6f99282e3fe677cd8f97ca89e
Tyler Huston <thuston@thecargoagency.com>
2016-08-04 15:42:57 -0400

    Add resources

---
+++ resources/assets/js/movie-api.js
@@ -0,0 +1,82 @@
+import { apiKey as apiKeyNew } from './keys';
+
+
+var demoList = new Vue({
+
+    el: '#mainContent',
+
+    data: {
+        items: [],
+        seasonNumber: null,
+        episodeNumber: null,
+        activeSeason: null
+    },
+
+    ready: function () {
+        this.fetchDataStart();
+    },
+
+    methods: {
+        fetchDataStart: function () {
+
+            var apiURL = "https://api.themoviedb.org/3/tv/1399?api_key=" + apiKeyNew + "&append_to_response=" + "images";
+
+            this.$http.get( apiURL, function( data ) {
+                this.items = data;
+
+                this.seasonNumber = this.items.seasons.length - 1;
+            });
+        },
+
+        changeSeason: function (number) {
+
+            var apiURL = "https://api.themoviedb.org/3/tv/1399/season/" + number +"?api_key=" + apiKeyNew + "&append_to_response=" + "images";
+
+            this.$http.get( apiURL, function( data ) {
+                this.items = data;
+                this.episodeNumber = this.items.episodes.length;
+            });
+
+            this.activeSeason = number;
+        },
+
+        changeEpisode: function (seasonNumber, number) {
+
+            var apiURL = "https://api.themoviedb.org/3/tv/1399/season/" + seasonNumber + "/episode/" + number + "?api_key=" + apiKeyNew + "&append_to_response=" + "images";
+
+            this.$http.get( apiURL, function( data ) {
+                this.items = data;
+            });
+        }
+    }
+
+    // methods: {
+    //     fetchDataStart: function () {
+    //
+    //         var apiURL = "https://api.themoviedb.org/3/tv/1399/season/1?api_key=" + apiKeyNew + "&append_to_response=" + "images";
+    //
+    //         this.$http.get( apiURL, function( data ) {
+    //             this.items = data;
+    //             this.episodeNumber = this.items.episodes.length;
+    //         });
+    //     },
+    //
+    //     changeSeason: function (number) {
+    //
+    //         var apiURL = "https://api.themoviedb.org/3/tv/1399/season/1/episode/" + number + "?api_key=" + apiKeyNew + "&append_to_response=" + "images";
+    //
+    //         this.$http.get( apiURL, function( data ) {
+    //             this.items = data;
+    //         });
+    //     },
+    //
+    //     changeEpisode: function (number) {
+    //
+    //         var apiURL = "https://api.themoviedb.org/3/tv/1399/season/1/episode/" + number + "?api_key=" + apiKeyNew + "&append_to_response=" + "images";
+    //
+    //         this.$http.get( apiURL, function( data ) {
+    //             this.items = data;
+    //         });
+    //     }
+    // }
+});
